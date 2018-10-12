05c4dc1028d1e10311c39a04ff69ec2834d71167
Becca Nelson <rnelson@8thlight.com>
2018-10-12 15:20:01 -0500

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
-/**
- * @class ExampleComponent
- */
+import * as React from "react";
 
-import * as React from 'react'
+import "./styles.scss";
 
-import styles from './styles.css'
-
-export type Props = { text: string }
-
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export interface IRowProps {
+  className?: string;
+  columns: number;
 }
+
+export interface IColumnProps {
+  className?: string;
+  sm?: number;
+  md?: number;
+  lg?: number;
+}
+
+export const Row: React.SFC<IRowProps> = ({ columns, children, className }) => {
+  const classNames = [`o-grid-${columns}-columns`];
+  if (className) {
+    classNames.push(className);
+  }
+
+  return <div className={classNames.join(" ")}>{children}</div>;
+};
+
+export const Column: React.SFC<IColumnProps> = ({
+  sm,
+  md,
+  lg,
+  children,
+  className
+}) => {
+  const classNames = [];
+  if (className) {
+    classNames.push(className);
+  }
+  if (sm) {
+    classNames.push(`o-span-${sm}`);
+  }
+  if (md) {
+    classNames.push(`o-span-${md}-med`);
+  }
+  if (lg) {
+    classNames.push(`o-span-${lg}-lg`);
+  }
+  return <div className={classNames.join(" ")}>{children}</div>;
+};
